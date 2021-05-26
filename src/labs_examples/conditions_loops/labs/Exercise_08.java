35fad1689b32568ceede319fd95cd61f0bdd89e1
Cruise87 <nicholaswaynick@yahoo.com>
2021-05-26 11:08:19 -0400

    Half of lab exercises for "condition_loops"

--- src/labs_examples/conditions_loops/labs/Exercise_08.java
+++ src/labs_examples/conditions_loops/labs/Exercise_08.java
@@ -8,4 +8,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_08 {
+    public static void main(String[] args) {
+
+
+        int x = 1000;
+        do {
+            x = x - 3;
+            System.out.println(x);
+
+        } while (x > 3);
+    }
 }
--- src/labs_examples/conditions_loops/labs/Exercise_09.java
+++ src/labs_examples/conditions_loops/labs/Exercise_09.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.sql.SQLOutput;
+
 /**
  * Conditions and Loops Exercise 9: break
  *
@@ -8,4 +10,11 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++){
+            System.out.println("i is: " + i);
+            if(i == 50)
+                break;
+        }
+    }
 }
