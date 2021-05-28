412e3aa20f0f799924fb1dc22f7dda45e627acfb
Cruise87 <nicholaswaynick@yahoo.com>
2021-05-28 07:27:27 -0400

    last 2 lab exercises for lab condition_loops

--- src/labs_examples/conditions_loops/labs/Exercise_11.java
+++ src/labs_examples/conditions_loops/labs/Exercise_11.java
@@ -22,5 +22,12 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_11 {
-
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print((i + 1) * j + " |");
+            }
+            System.out.println();
+        }
+    }
 }
